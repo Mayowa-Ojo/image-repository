2c6ca5ea1d86f678b527e9e0935bd1b33f0bf9bb
Mayowa Ojo <mayowaojo.e@gmail.com>
2021-01-11 15:03:30 -0700

    :sparkles: [feat]: register middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,34 @@
 import express from "express";
 import helmet from "helmet";
 import cors from "cors";
-import winston from "winston";
-import type { Request, Response } from "express";
+import morgan from "morgan";
 
-import { logger } from "~config/logger.config";
+import baseRouter from "~routes/index";
+import { stream } from "~config/logger.config";
 import { notFoundError, errorHandler } from "~middleware/error.middleware";
 
 const app = express();
 
 app.use(helmet());
+
 app.use(cors({
    methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
    origin: "*"
 }));
-app.use(logger(winston));
+
+app.use(morgan("dev", {
+   stream,
+   skip: (_req, res) => res.statusCode < 200 || res.statusCode >= 400
+}));
+
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (_req: Request, res: Response) => {
-   res.status(200).json({
-      ok: true,
-      message: "welcome message",
-      data: {}
-   });
-});
+app.use("/api/v1", baseRouter);
 
 app.use(notFoundError);
+
 app.use(errorHandler);
 
 export default app;
