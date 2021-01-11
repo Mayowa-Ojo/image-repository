2e69db314498fa54de4d5fc284dfd3c63143684e
Mayowa Ojo <mayowaojo.e@gmail.com>
2021-01-11 14:57:59 -0700

    :wrench: [config]: setup jest with typescript

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,11 @@ module.exports = {
       "~routes/(.*)": "<rootDir>/src/routes/$1",
       "~config/(.*)": "<rootDir>/src/config/$1",
       "~middleware/(.*)": "<rootDir>/src/middleware/$1",
-      "~declarations/(.*)": "<rootDir>/src/declarations/$1"
+      "~declarations/(.*)": "<rootDir>/src/declarations/$1",
+      "~controllers/(.*)": "<rootDir>/src/controllers/$1",
+      "~utils/(.*)": "<rootDir>/src/utils/$1",
+      "~entity/(.*)": "<rootDir>/src/database/entity/$1",
+      "~repository/(.*)": "<rootDir>/src/database/repository/$1"
    },
    testEnvironment: "node",
 };
