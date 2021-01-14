32441487fedd3d3be250c633c6ed5096e6d67634
Mayowa Ojo <mayowaojo.e@gmail.com>
2021-01-14 09:03:52 -0700

    :recycle: [refactor]: override database name for test env

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,11 +1,20 @@
-import { createConnection } from "typeorm";
+import { createConnection, getConnectionOptions, Connection } from "typeorm";
 
-const connectDB = async () => {
+import { config } from "~config/env.config";
+
+const connectDB = async (): Promise<Connection | void> => {
 
    try {
-      await createConnection();
+      const options = await getConnectionOptions();
+
+      if(config.NODE_ENV === "test") {
+         Object.assign(options, { database: config.DB_TEST});
+      }
+
+      const connection = await createConnection(options);
 
       console.log("[INFO] --typeorm: connected to database");
+      return connection;
    } catch (err) {
       console.error(`[ERROR] --typeorm: ${err.message}`);
    }
